e2172335be7e34fcc8912b4657e098b0486000ff
Leonardo Caycho <leonardo_caycho@usmp.pe>
2023-08-25 13:42:03 -0500

    Primer commit

---
+++ Models/Contacto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace petclinic.Models
+{
+    public class Contacto
+    {
+        public string? Name {get; set;}
+        public string? Email {get; set;}
+        public string? Phone {get; set;}
+        public string? Question {get; set;}
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace petclinic.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
