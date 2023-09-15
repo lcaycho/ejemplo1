8bc7b26d85acbe1bcddf0866c6e2aec5a13fa104
Leonardo Caycho <leonardo_caycho@usmp.pe>
2023-09-15 15:19:31 -0500

    Catalogo

--- Data/Migrations/20230915194743_ProductoMigration.Designer.cs
+++ Data/Migrations/20230915194743_ProductoMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using petclinic.Data;
@@ -11,9 +12,11 @@ using petclinic.Data;
 namespace petclinic.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230915194743_ProductoMigration")]
+    partial class ProductoMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
